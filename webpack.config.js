06d3422f64963e82c0c192540409c5edd7183546
Andrii Pravnyk <andrii.pravnyk@Blackthorn-vision.com>
2023-07-26 10:39:05 +0300

    TARGET

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,7 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-// const TARGET = process.env.TARGET;
-const TARGET = 'dev';
+const TARGET = process.env.TARGET;
 const ROOT_PATH = path.resolve(__dirname);
 const nodeModulesDir = path.join(ROOT_PATH, 'node_modules');
 
